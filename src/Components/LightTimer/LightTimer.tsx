c6696eef21e257f5fa073ee1838e00aa9e50168b
Heldereim <adam.hellgard@hotmail.se>
2024-09-30 16:25:45 +0200

    Finished building the contentpane

--- src/Components/LightTimer/LightTimer.tsx
+++ src/Components/LightTimer/LightTimer.tsx
@@ -1,11 +1,44 @@
 import React from "react";
+import Clock from "./Clock";
+import DimmingTimer from "./DimmingTimer";
+import Button from "../Button/Button";
 
 const LightTimer = () => {
   return (
     <>
-      <div className="p-3 mb-2 bg-primary text-secondary">LightTimer</div>
+      <div className="container p-3 mb-2">
+        <div className="m-3 p-2 container col bg-black text-white">
+          <h1
+            className="p-2 m-3 bg-secondary text-white"
+            children={DimmingTimer(19, 8)}
+          ></h1>
+          <div
+            className="p-2 m-3 bg-secondary text-white"
+            children={Clock(19, 8, 22, 20)}
+          />
+          <div className="p-1 m-3 bg-secondary text-white row">
+            <div className="col-sm h-50">
+              <Button
+                styling="btn btn-secondary"
+                onClicked={DimLights}
+                children={<h2>Start now</h2>}
+              />
+            </div>
+            <div className="col-sm h-50">
+              <Button
+                styling="btn btn-secondary align-middle"
+                onClicked={PauseDim}
+                children={<h2>Pause</h2>}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
+
+const DimLights = () => void {};
+const PauseDim = () => void {};
 
 export default LightTimer;
